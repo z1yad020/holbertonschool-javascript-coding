9065c69bf210310129c511e51946c1b35adc839c
ziyad <ziyad.ibrahim.112358@gmail.com>
2024-05-19 14:05:54 +0000

    task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-  console.error('error:', error);
+  if (error) console.error('error:', error);
   console.log('code:', response.statusCode);
 });
