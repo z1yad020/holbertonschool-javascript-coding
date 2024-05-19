f3075fc25d3c30047f8e729626b4899b1e654569
ziyad <ziyad.ibrahim.112358@gmail.com>
2024-05-19 19:10:47 +0000

    foreaches

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,24 @@
 #!/usr/bin/node
-
+/* eslint-disable no-unused-vars */
+const { count } = require('console');
 const request = require('request');
 
-const url = `https://swapi-api.hbtn.io/api/people/18`;
+const url = process.argv[2];
 
 request(url, function (error, response, body) {
-  if (error) console.error('error:', error);
-  console.log(JSON.parse(body).films.length);
+  if (error) {
+    console.error(error);
+  }
+
+  const films = JSON.parse(body).results;
+  let count = 0;
+  films.forEach((film) => {
+    const filteredCharacters = film.characters.filter((character) =>
+      character.includes('18')
+    );
+    if (filteredCharacters.length > 0) {
+      count++;
+    }
+  });
+  console.log(count);
 });
