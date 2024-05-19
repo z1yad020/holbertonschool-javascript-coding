2c6dce930d4893272cb915b0bba8cc863c0e0bc4
ziyad <ziyad.ibrahim.112358@gmail.com>
2024-05-19 13:55:32 +0000

    task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
