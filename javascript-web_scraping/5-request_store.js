ed6aefaec9f6a9d9bc875d6fcdd62341edfc20d9
ziyad <ziyad.ibrahim.112358@gmail.com>
2024-05-19 19:46:57 +0000

    semi

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,4 +3,4 @@
 const request = require('request');
 const fs = require('fs');
 
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]))
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
