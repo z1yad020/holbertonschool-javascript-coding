682bd95e8b2016b5241216405f753686e680064b
ziyad <ziyad.ibrahim.112358@gmail.com>
2024-05-19 13:56:32 +0000

    remove print

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,6 +3,5 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
+  if (err) throw err; 
 });
